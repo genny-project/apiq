57709d4ff116c45538161c48c8d3fc111698f857
Byron Andres <heliosphanes@outlook.com>
2020-08-24 15:36:03 +1000

    quarkus pom added 1.7.0.Final

---
+++ src/test/java/life/genny/apiq/NativeEndpointsIT.java
@@ -0,0 +1,9 @@
+package life.genny.apiq;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeEndpointsIT extends EndpointsTest {
+
+    // Execute the same tests but in native mode.
+}
