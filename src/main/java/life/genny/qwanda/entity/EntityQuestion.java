50ade032ce64586b7879e27334a10bd43eb9d821
Adam Crow <adamcrow63@gmail.com>
2020-08-30 20:57:47 +1000

    Working

--- src/main/java/life/genny/qwanda/entity/EntityQuestion.java
+++ src/main/java/life/genny/qwanda/entity/EntityQuestion.java
@@ -2,11 +2,12 @@ package life.genny.qwanda.entity;
 
 import java.io.Serializable;
 
+import javax.json.bind.annotation.JsonbTransient;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.Table;
-
-import io.quarkus.hibernate.orm.panache.PanacheEntity;
-import io.quarkus.runtime.annotations.RegisterForReflection;
+import javax.persistence.Transient;
+import javax.validation.constraints.NotNull;
 
 //
 //import javax.json.bind.annotation.JsonbTransient;
@@ -17,6 +18,10 @@ import io.quarkus.runtime.annotations.RegisterForReflection;
 //import javax.validation.constraints.NotNull;
 //
 import org.jboss.logging.Logger;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import io.quarkus.runtime.annotations.RegisterForReflection;
+import life.genny.qwanda.Value;
 //
 //import io.quarkus.runtime.annotations.RegisterForReflection;
 //import life.genny.qwanda.Value;
@@ -30,9 +35,9 @@ public class EntityQuestion  extends PanacheEntity implements Serializable, Comp
 
 	private static final Logger log = Logger.getLogger(EntityQuestion.class);
 //
-//	@Embedded
-//	@NotNull
-//	public Value value;
+	@Embedded
+	@NotNull
+	public Value value;
 //
 ////	private Link link;
 //
@@ -47,28 +52,28 @@ public class EntityQuestion  extends PanacheEntity implements Serializable, Comp
 	public int compareTo(Object o) {
 		return 0;
 	}
-//
-//	@SuppressWarnings("unchecked")
-//	@JsonbTransient
-//	@Transient
-//	public <T> T getValue() {
-//		return value.getValue();
-//
-//	}
-//
-//	@JsonbTransient
-//	@Transient
-//	public <T> void setValue(final Object value) {
-//		this.value.setValue(value);
-//	}
-//
-//	@Transient
-//	public Double getWeight() {
-//		return value.weight;
-//	}
-//
-//	@Transient
-//	public void setWeight(Double weight) {
-//		value.weight = weight;
-//	}
+
+	@SuppressWarnings("unchecked")
+	@JsonbTransient
+	@Transient
+	public <T> T getValue() {
+		return value.getValue();
+
+	}
+
+	@JsonbTransient
+	@Transient
+	public <T> void setValue(final Object value) {
+		this.value.setValue(value);
+	}
+
+	@Transient
+	public Double getWeight() {
+		return value.weight;
+	}
+
+	@Transient
+	public void setWeight(Double weight) {
+		value.weight = weight;
+	}
 }
